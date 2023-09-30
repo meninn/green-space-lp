eeb46c91ea3d90e7e5b03a2f652e78bdb58d2672
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-30 17:24:42 -0300

    early build of the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,16 @@ import './styles.scss';
 
 const Footer = () => {
   return(
-    <footer>
-      Place holder text from the footer
+    <footer className='footer__menu'>
+        <ul>
+            <li className='footer__logo'><a href="#"><img src="src/assets/svg/Logo-footer.svg" alt="GreenSpace logo" /></a></li>
+            <li><a href="#"><img src="src/assets/svg/Github ico - footer.svg" alt="GitHub icon with the name of the devs" />Gustavo</a></li>
+            <li><a href="#"><img src="src/assets/svg/Github ico - footer.svg" alt="GitHub icon with the name of the devs" />Guilherme</a></li>
+            <li><a href="#"><img src="src/assets/svg/Github ico - footer.svg" alt="GitHub icon with the name of the devs" />Kauan</a></li>
+            <li><a href="#"><img src="src/assets/svg/Github ico - footer.svg" alt="GitHub icon with the name of the devs" />Vinicius</a></li>
+            <li><a href="#"><img src="src/assets/svg/Github ico - footer.svg" alt="GitHub icon with the name of the devs" />Gabriel</a></li>
+            <li className='footer__logo'><a href="#"><img src="src/assets/svg/CesuscLogo.svg" alt="Cesusc Logo" /></a></li>
+        </ul>
   </footer>
   )
 }
