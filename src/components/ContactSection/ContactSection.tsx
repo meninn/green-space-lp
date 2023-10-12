2e97e4a0b878eaed051ce251450ff4f5bdebb8b4
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-10-12 06:54:03 -0300

    feat: contact section text

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,7 +1,35 @@
-import './styles.scss'
+import "./styles.scss";
 
-const ContactSection = () =>{
-    return (<div className='contact__section'>Contact Section placeholder text</div>)
-}
+import FigmaIcon from "../../assets/svg/FIGMA ICO.svg";
+import GithubIcon from "../../assets/svg/GIGTHUB ICO.svg";
 
-export default ContactSection
+const ContactSection = () => {
+  return (
+    <section className="contact__section">
+      <div className="contact__section__header">
+        <h2>FIGMA PROTO &&</h2>
+        <h2>GITHUB REPO</h2>
+        <h4>Prototipo da aplicação referente ao projeto Green Space</h4>
+      </div>
+
+      <div className="contact__group">
+        <div className="contact__group__link">
+          <img src={FigmaIcon} alt="Figma's logo" />
+          <p>
+            Clique aqui para <br />
+            ver o nosso figma
+          </p>
+        </div>
+        <div className="contact__group__link">
+          <img src={GithubIcon} alt="Github's logo" />
+          <p>
+            Clique aqui para <br />
+            ver o nosso github
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
