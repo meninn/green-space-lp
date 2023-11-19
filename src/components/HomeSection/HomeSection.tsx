8a5e022829c55ae6dcdb1d6b0285b16131b56644
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-19 01:28:56 -0300

    it's finished, it's beaultiful but don't try resizing the window please

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -4,9 +4,22 @@ const HomeSection = () => {
   return (
     <section className="home__section">
       <div className="main__content home__content">
+
         <div className="home__content__text">
           <h2>GREEN SPACE</h2>
           <p>O melhor lugar para se plantar ideias</p>
+
+        <div className="contact__group">
+          <div className="contact__group__button">
+            <img src="src/assets/svg/logo-without-name.svg" alt="GreenSpace logo" />
+            <p>
+              Clique aqui para <br />
+              conhecer a <br/>
+              nossa rede
+            </p>
+          </div>
+        </div>
+
         </div>
 
         <div className="home__content__image">
