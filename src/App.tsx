2873670565d29bb6e0d735bb79a9d18adb8a3bbf
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-11-19 16:14:11 -0300

    to the top button done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import HomeSection from "./components/HomeSection";
 import AboutSection from "./components/AboutSection";
 import BusinessModelSection from "./components/BusinessModelSection";
 import ContactSection from "./components/ContactSection";
-import Footer from "./components/Footer"
+import TopButton from "./components/TopButton";
+import Footer from "./components/Footer";
 
 import "./styles/global.scss";
 
@@ -13,7 +14,8 @@ const App = () => {
       <AboutSection />
       <BusinessModelSection />
       <ContactSection />
-      <Footer/>
+      <Footer />
+      <TopButton />
     </div>
   );
 };
---
+++ src/components/TopButton/TopButton.tsx
@@ -0,0 +1,26 @@
+import TopArrow from "../../assets/svg/goToTopArrow.svg";
+import "./styles.scss";
+
+const TopButton = () => {
+  const returnToTop = () => {
+    if (document) {
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <button
+      className="top-btn"
+      onClick={() => {
+        returnToTop();
+      }}
+    >
+      <img className="top-btn__img" src={TopArrow} alt="arrow up"></img>
+    </button>
+  );
+};
+
+export default TopButton;
